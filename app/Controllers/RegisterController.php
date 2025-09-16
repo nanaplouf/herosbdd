8248222d84b54ff4527d72698c0300554ebbef10
nanaplouf <vera78ds@gmail.com>
2025-09-16 16:32:08 +0200

    creation d'un user

--- app/Controllers/RegisterController.php
+++ app/Controllers/RegisterController.php
@@ -62,7 +62,7 @@ if(isset($_POST['pseudo'])){
 
     checkFormat('pseudo', $valuePseudo);
     checkFormat('password', $valuePassword);
-    checkFormat('email', $valuePseudo);
+    checkFormat('email', $valueEmail);
 
     isNotEmpty('pseudo');
     isNotEmpty('email');
@@ -70,6 +70,26 @@ if(isset($_POST['pseudo'])){
     
     //nos erreurs sont dans :
     var_dump($arrayError);
+    
+    //Si mon tableau d'erreur est vide alor :
+    if(empty($arrayError)){
+
+        // 0- Je creer la requête SQL:
+        $query = "INSERT INTO `user` (`pseudo`, `password`, `email`) 
+        VALUES (:pseudo, :password, :email)";
+
+        // 1- prépare la requête :
+        $queryStatement = $pdo->prepare($query);
+
+        // 2- lier les marqueurs aux valeurs :
+        $queryStatement->bindValue(':pseudo', $valuePseudo);
+        $queryStatement->bindValue(':password', $valuePassword);
+        $queryStatement->bindValue(':email', $valueEmail);
+
+        // 3- exécuter la requête :
+        $queryStatement->execute();
+
+    }
 
     require_once( __DIR__ . "/../Views/register.view.php" );
 }else{
